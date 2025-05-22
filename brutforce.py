2f5551fef32e09ed62ae626a9bef4b01c7fd6def
Johmmy29 <130465190+Johmmy29@users.noreply.github.com>
2025-05-22 19:02:08 +0200

    Update brutforce.py

--- brutforce.py
+++ brutforce.py
@@ -1,7 +1,7 @@
 import numpy as np
 import itertools
 
-# Matrice des distances
+# Matrice des distances de notre exemple
 D = np.array([
     [0, 12, 9, 24, 18, 14],
     [12, 0, 7, 20, 15, 21],
@@ -19,7 +19,7 @@ def calcul_cout_tour(tour, D):
 
 def tsp_force_brute(D):
     n = D.shape[0]
-    villes = list(range(1, n))  # on fixe la ville 0 comme départ
+    villes = list(range(1, n)) 
     cout_min = float('inf')
     meilleur_tour = None
 
@@ -33,5 +33,5 @@ def tsp_force_brute(D):
     return meilleur_tour, cout_min
 
 tour_optimal, cout_optimal = tsp_force_brute(D)
-print("Tour optimal :", [v+1 for v in tour_optimal])  # +1 pour afficher de 1 à 8
+print("Tour optimal :", [v+1 for v in tour_optimal])
 print("Coût optimal :", cout_optimal)
